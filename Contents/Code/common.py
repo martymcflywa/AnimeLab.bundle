8a570e3bec10960be864cee2efdf77b7b8b60414
martymcflywa <doc.brown.83@gmail.com>
2017-08-26 12:24:44 +0800

    Login test using requests package.

---
+++ Contents/Code/common.py
@@ -0,0 +1,9 @@
+TITLE = 'AnimeLab'
+VERSION = '0.01' # Release notation (x.y - where x is major and y is minor)
+GITHUB_REPOSITORY = 'martymcflywa/AnimeLab.bundle'
+PREFIX = '/video/animelab'
+
+BASE_URL = 'https://www.animelab.com'
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 5_1 like Mac OS X; en-us) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B176 Safari/7534.48.3'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
